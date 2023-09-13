7eeda1962da06e4d9330747771838c8f530a1429
Natan Foleto <natanfoleto2015@hotmail.com>
2023-09-12 22:21:14 -0300

    Add users on response create reaction

--- src/modules/posts/repositories/PostRepository.ts
+++ src/modules/posts/repositories/PostRepository.ts
@@ -47,14 +47,6 @@ class PostRepository implements IPostsRepositories {
         tags: true,
         visibility: true,
         published_at: true,
-        users: {
-          select: {
-            id: true,
-            name: true,
-            email: true,
-            avatar_url: true,
-          },
-        },
       },
     });
   }
